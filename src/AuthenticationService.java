5688df4e2c52235d1033991e4a92bb2a8b7a7010
guessurmine <trjstewie@outlook.com>
2025-03-09 13:02:11 +0800

    Some message

---
+++ src/AuthenticationService.java
@@ -0,0 +1,15 @@
+﻿import java.util.ArrayList;
+
+public class AuthenticationService implements IAuthenticationService {
+    private ArrayList<User> users;
+
+    @Override
+    public User signUp(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public User logIn(String username, String password) {
+        return null;
+    }
+}
